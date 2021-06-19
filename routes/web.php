a021049ccd2a90307da22febe6ba28362a0a1362
soaika74 <hoviethung10b2@gmail.com>
2021-06-19 13:43:39 +0700

    hung upload

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('', [ App\Http\Controllers\Website\HomeController::class,'index' ]);
+
