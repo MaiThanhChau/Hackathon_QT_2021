bf3771bd0ec60bd4e1a4fff4f5b18f631fc45d77
taitamfc <taitam8590@gmail.com>
2021-06-20 13:16:31 +0700

    fix curren user id 2

--- app/Http/Controllers/Website/HomeController.php
+++ app/Http/Controllers/Website/HomeController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 use App\Models\Chats;
 use App\Models\Question;
+use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller
 {
     public function index(){ 
